d780ba873e661cdf521b6ea0087ad31eaf2632c5
Kaiusz <mrkgyngysi@gmail.com>
2021-04-08 21:12:15 +0300

    configured main class

--- src/main/java/pkg/Main.java
+++ src/main/java/pkg/Main.java
@@ -2,22 +2,34 @@ package pkg;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+import org.apache.logging.log4j.ThreadContext;
 
 public class Main {
 
-    private static final Logger logger = LogManager.getLogger();
+    private static final Logger logger = LogManager.getLogger(Main.class.getName());
 
-    public static void main(String[] args) {
-        logger.fatal("This is a FATAL message");
-        logger.error("This is an ERROR message");
-        logger.warn("This is a WARN message");
-        logger.info("This is an INFO message");
-        logger.debug("This is a DEBUG message");
-        logger.trace("This is a TRACE message");
-        logger.info("Java version is {}", System.getProperty("java.version"));
-        logger.trace("Available free memory: {} kB", () -> Runtime.getRuntime().freeMemory() / 1024);
-        logger.error("An exception occurred", new RuntimeException("Oops, something went wrong"));
-        logger.error("This is an ERROR message that contains 'FAIL'");
+    private static final Marker QUESTION_MARKER = MarkerManager.getMarker("question");
+    private static final Marker STATEMENT_MARKER = MarkerManager.getMarker("statement");
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 1; i <= Integer.parseInt(args[0]); i++) {
+            ThreadContext.push(String.valueOf(i));
+            logger.fatal(STATEMENT_MARKER, "Korán reggel útra keltem,");
+            logger.error(STATEMENT_MARKER, "Se nem ittam, se nem ettem.");
+            logger.warn(STATEMENT_MARKER, "Tarisznya húzza a vállam,");
+            logger.info(STATEMENT_MARKER, "Térdig kopott már a lábam.");
+            logger.debug(STATEMENT_MARKER, "Bejártam a fél világot,");
+            logger.trace(STATEMENT_MARKER, "Láttam sok-sok szép virágot.");
+            logger.fatal(STATEMENT_MARKER, "A legszebbre most találtam,");
+            logger.error("Hogy öntözzem, alig vártam.");
+            logger.info(QUESTION_MARKER, "Szabad-e locsolni?");
+            Thread.sleep(3000);
+            ThreadContext.pop();
+        }
     }
-
 }
+
+
